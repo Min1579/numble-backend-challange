8dc28ea83004c1b0af1da7f0a84304be680970f2
Min1579 <hsm012362@gmail.com>
2022-01-07 17:43:56 +0900

    user create test

---
+++ src/test/java/me/min/karrotmarket/user/UserRepositoryTest.java
@@ -0,0 +1,47 @@
+package me.min.karrotmarket.user;
+
+import me.min.karrotmarket.user.model.User;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataJpaTest
+@ExtendWith(SpringExtension.class)
+class UserRepositoryTest {
+    @Autowired
+    private UserRepository userRepository;
+
+    @Test
+    public void create_user_test() {
+        // given
+        final String email = "hsm012362@gamil.com";
+        final String password = "password";
+        final String name = "승민";
+        final String phoneNumber = "010-3359-1321";
+        final String nickname = "Min";
+
+        // when
+        User user = User.builder()
+                .email(email)
+                .password(password)
+                .name(name)
+                .phoneNumber(phoneNumber)
+                .nickname(nickname).build();
+
+        User actual = userRepository.save(user);
+
+        //then
+        assertThat(actual.getId()).isNotNull();
+        assertThat(actual.getCreatedAt()).isNotNull();
+        assertThat(actual.getUpdatedAt()).isNotNull();
+        assertThat(actual.getDeletedAt()).isNull();
+        assertThat(actual.getEmail()).isEqualTo(email);
+        assertThat(actual.getPassword()).isEqualTo(password);
+        assertThat(actual.getName()).isEqualTo(name);
+        assertThat(actual.getNickname()).isEqualTo(nickname);
+    }
+}
