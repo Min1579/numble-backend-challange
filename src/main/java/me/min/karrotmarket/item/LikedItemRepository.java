e5514a682a1a9f842119029f80a66632cbfb1781
Min1579 <hsm012362@gmail.com>
2022-01-07 16:54:22 +0900

    add liked item entity & repository

---
+++ src/main/java/me/min/karrotmarket/item/LikedItemRepository.java
@@ -0,0 +1,6 @@
+package me.min.karrotmarket.item;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LikedItemRepository extends JpaRepository<LikedItem, Long> {
+}
