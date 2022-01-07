34d1524f875c46827862328c78f8be63f252adfa
Min1579 <hsm012362@gmail.com>
2022-01-07 15:58:40 +0900

    project init

---
+++ src/main/java/me/min/karrotmarket/KarrotMarketApplication.java
@@ -0,0 +1,13 @@
+package me.min.karrotmarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KarrotMarketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KarrotMarketApplication.class, args);
+    }
+
+}
