7d021f366f6872d50aa4185b3edb9115e536770f
Min1579 <hsm012362@gmail.com>
2022-01-17 01:57:42 +0900

    refactor code

--- src/main/java/me/min/karrotmarket/item/ItemController.java
+++ src/main/java/me/min/karrotmarket/item/ItemController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @RequiredArgsConstructor
@@ -26,21 +27,21 @@ public class ItemController {
 
     @PostMapping
     public ResponseEntity<Long> createItem(@Authentication final CurrentUser user,
-                                           @RequestBody ItemCreatePayload payload) {
+                                           @Valid @RequestBody ItemCreatePayload payload) {
         return new ResponseEntity<>(itemService.createItem(user.getId(), payload), HttpStatus.CREATED);
     }
 
     @PatchMapping("{itemId}")
     public ResponseEntity<Long> updateItemStatus(@Authentication final CurrentUser user,
                                                  @PathVariable("itemId") final Long itemId,
-                                                 @RequestBody ItemStatusUpdatePayload payload) {
+                                                 @Valid @RequestBody ItemStatusUpdatePayload payload) {
         return new ResponseEntity<>(itemService.updateItemStatus(user.getId(), itemId, payload), HttpStatus.CREATED);
     }
 
     @PutMapping("{itemId}")
     public ResponseEntity<Long> updateItem(@Authentication final CurrentUser user,
                                            @PathVariable("itemId") final Long itemId,
-                                           @RequestBody ItemUpdatePayload payload) {
+                                           @Valid @RequestBody ItemUpdatePayload payload) {
         return new ResponseEntity<>(itemService.updateItem(user.getId(), itemId, payload), HttpStatus.CREATED);
     }
 
@@ -53,7 +54,7 @@ public class ItemController {
     @PostMapping("{itemId}/comment")
     public ResponseEntity<Long> createItemComment(@Authentication final CurrentUser user,
                                                   @PathVariable("itemId") final Long itemId,
-                                                  @RequestBody final ItemCommentCreatePayload payload) {
+                                                  @Valid @RequestBody final ItemCommentCreatePayload payload) {
         return new ResponseEntity<>(itemCommentService.createItemComment(user.getId(), itemId, payload), HttpStatus.CREATED);
     }
 
@@ -67,7 +68,7 @@ public class ItemController {
     @PutMapping("comment/{commentId}")
     public ResponseEntity<Void> updateItemComment(@Authentication final CurrentUser user,
                                                   @PathVariable("commentId") final Long commentId,
-                                                  @RequestBody final ItemCommentUpdatePayload payload) {
+                                                  @Valid @RequestBody final ItemCommentUpdatePayload payload) {
         itemCommentService.updateItemComment(user.getId(), commentId, payload);
         return ResponseEntity.ok().build();
     }
