4924aadf2da4456f05b5b1d5befa33efbb3481db
Min1579 <hsm012362@gmail.com>
2022-01-23 16:59:05 +0900

    refactor code

--- src/main/java/me/min/karrotmarket/KarrotMarketApplication.java
+++ src/main/java/me/min/karrotmarket/KarrotMarketApplication.java
@@ -4,10 +4,6 @@ import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import lombok.RequiredArgsConstructor;
-import me.min.karrotmarket.user.UserRepository;
-import me.min.karrotmarket.user.model.User;
-import org.springframework.boot.ApplicationArguments;
-import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -15,13 +11,10 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import java.beans.BeanProperty;
-
 @RequiredArgsConstructor
 @EnableJpaAuditing
 @SpringBootApplication
-public class KarrotMarketApplication implements ApplicationRunner, WebMvcConfigurer {
-    private final UserRepository userRepository;
+public class KarrotMarketApplication implements WebMvcConfigurer {
 
     public static void main(String[] args) {
         SpringApplication.run(KarrotMarketApplication.class, args);
@@ -37,23 +30,10 @@ public class KarrotMarketApplication implements ApplicationRunner, WebMvcConfigu
 
     @Bean
     public OpenAPI customOpenAPI() {
+        final SecurityScheme securityScheme
+                = new SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT");
         final Components components = new Components()
-                .addSecuritySchemes("bearer-key",
-                new SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT"));
+                .addSecuritySchemes("bearer-key", securityScheme);
         return new OpenAPI().components(components);
-    }
-
-    @BeanProperty
-
-    @Override
-    public void run(ApplicationArguments args) {
-        final User user = User.builder()
-                .email("hsm012362@gmail.com")
-                .password("$2a$10$xDFCfHptj7Axu56fYzqmpusZs8yz3gf..ePCVSrIjW2gvETqETv2a")
-                .nickname("min")
-                .name("min")
-                .phoneNumber("010-3359-1321")
-                .build();
-        userRepository.save(user);
     }
 }
