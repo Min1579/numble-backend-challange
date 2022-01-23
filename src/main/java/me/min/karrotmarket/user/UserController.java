bc8ce2f0c6cbd779080f3790b4fb2ac3e5553d97
Min1579 <hsm012362@gmail.com>
2022-01-23 16:58:04 +0900

    add summary

--- src/main/java/me/min/karrotmarket/user/UserController.java
+++ src/main/java/me/min/karrotmarket/user/UserController.java
@@ -1,9 +1,14 @@
 package me.min.karrotmarket.user;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import lombok.RequiredArgsConstructor;
+import me.min.karrotmarket.security.Authentication;
+import me.min.karrotmarket.security.CurrentUser;
 import me.min.karrotmarket.user.mapper.LoginResponseMapper;
 import me.min.karrotmarket.user.payload.UserCreatePayload;
 import me.min.karrotmarket.user.payload.UserAuthPayload;
+import me.min.karrotmarket.user.payload.UserUpdatePayload;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -15,15 +20,24 @@ import javax.validation.Valid;
 @RequestMapping("api/v1/user")
 @RestController
 public class UserController {
-    private final UserServiceImpl userService;
+    private final UserService userService;
 
     @PostMapping("join")
+    @Operation(summary = "회원가입")
     public ResponseEntity<Long> createUser(@Valid @RequestBody final UserCreatePayload payload) {
         return new ResponseEntity<>(userService.createUser(payload), HttpStatus.CREATED);
     }
 
     @PostMapping("login")
+    @Operation(summary = "로그인")
     public ResponseEntity<LoginResponseMapper> login(@Valid @RequestBody final UserAuthPayload payload) {
         return ResponseEntity.ok(userService.login(payload));
+    }
+
+    @PutMapping()
+    @Operation(security = {@SecurityRequirement(name = "bearer-key")}, summary = "유저 업데이트")
+    public ResponseEntity<Long> updateUser(@Authentication final CurrentUser user,
+                                           @Valid@RequestBody final UserUpdatePayload payload) {
+        return ResponseEntity.ok(userService.updateUser(user.getId(), payload));
     }
 }
