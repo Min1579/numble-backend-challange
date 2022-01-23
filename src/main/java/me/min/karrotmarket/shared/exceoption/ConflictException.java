6fdc8567eaa1d3c9eba04429a87d7bce24c9e3c9
Min1579 <hsm012362@gmail.com>
2022-01-23 16:56:58 +0900

    edit exception

--- src/main/java/me/min/karrotmarket/shared/exceoption/ConflictException.java
+++ src/main/java/me/min/karrotmarket/shared/exceoption/ConflictException.java
@@ -4,8 +4,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(HttpStatus.CONFLICT)
-public class DuplicatedException extends RuntimeException {
-    public DuplicatedException(final String resource) {
+public class ConflictException extends RuntimeException {
+    public ConflictException(final String resource) {
         super(String.format("%s Not Found", resource));
     }
 }
